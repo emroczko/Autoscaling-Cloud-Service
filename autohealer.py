38cadb98b1706a5ad966198e8c3a45410dc4bee8
Eryk <emroczko@me.com>
2023-01-04 01:58:10 +0100

    Added autoscaling (upscaling for now) feature to script

--- autohealer.py
+++ autohealer.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 import subprocess
 import yaml
+from numpy import mean
 
 from yaml.loader import SafeLoader
 from io import StringIO
@@ -36,35 +37,90 @@ def autohealing():
 
 
 def autoscaling():
-    print("Checking autoscaling policy...")
 
-    minimum_instances = DEFAULT_MIN_INSTANCES
-    maximum_instances = DEFAULT_MAX_INSTANCES
+    while True:
+        print("Checking autoscaling policy...")
 
-    try:
-        with open('autoscaler_properties.yaml') as f:
-            data = yaml.load(f, Loader=SafeLoader)
-            minimum_instances = int(data['min_instances'])
-            maximum_instances = int(data['max_instances'])
-    except ValueError:
-        print("Expected ints; fix the file, or default values will be used")
-    except TypeError:
-        print("Properties file empty!")
-    except FileNotFoundError:
-        print("Properties file not found! using default values")
+        minimum_instances = DEFAULT_MIN_INSTANCES
+        maximum_instances = DEFAULT_MAX_INSTANCES
+
+        try:
+            with open('autoscaler_properties.yaml') as f:
+                data = yaml.load(f, Loader=SafeLoader)
+                minimum_instances = int(data['min_instances'])
+                maximum_instances = int(data['max_instances'])
+        except ValueError:
+            print("Expected ints; fix the file, or default values will be used")
+        except TypeError:
+            print("Properties file empty!")
+        except FileNotFoundError:
+            print("Properties file not found! using default values")
+
+        df = read_stats()
+        filtered = df[(df.type == 2)]
+        number_of_last_existing_vm = len(filtered)
+
+        if mean(filtered['rate']) > 16:
+            if len(filtered) < maximum_instances:
+                add_machine_to_pool(number_of_last_existing_vm + 1)
+                sleep(10)
+            else:
+                print("Maximum number of instances is already up")
+
+        if mean(filtered['rate']) < 5:
+            if len(filtered) > minimum_instances:
+                remove_machine_from_pool(number_of_last_existing_vm)
+                sleep(10)
+            else:
+                print("Minimum number of instances is already up")
+
+        sleep(10)
+
+def remove_machine_from_pool(vm_num: int):
+    vm_num = str(vm_num) if vm_num > 9 else f'0{vm_num}'
+    subprocess.call(["vagrant", "destroy", "-f", f'web{vm_num}'])
+
+
+
+
+def add_machine_to_pool(vm_num: int):
+    vm_num = str(vm_num) if vm_num > 9 else f'0{vm_num}'
+    vm_declaration = """
+  config.vm.define "web{0}" do |web|
+    web.vm.box = "ubuntu/focal64"
+    web.vm.network "private_network", ip: "192.168.50.1{1}"
+    web.vm.provision "shell", path: "install_web.sh", args: "web{2}"
+  end
+    """.format(vm_num, vm_num, vm_num)
+
+    with open('Vagrantfile', 'r') as vagrant_file:
+        vagrant_lines = vagrant_file.readlines()
+
+    vagrant_lines.insert(-1, vm_declaration)
+    with open('Vagrantfile', 'w') as vagrant_file:
+        vagrant_file.writelines(vagrant_lines)
+
+    print(f'Creating vm web{vm_num} again...')
+    subprocess.call(["vagrant", "up", f"web{vm_num}"])
+
+    with open('haproxy.cfg', 'a') as haproxy:
+        haproxy.write(f'    server  web3 192.168.50.1{vm_num}:9103 check')
+
+    subprocess.call(["haproxy", "-f", "haproxy.cfg"])
+
+    sleep(10)
 
     df = read_stats()
     filtered = df[(df.type == 2)]
 
-    for session_rate in filtered['rate']:
-        if session_rate > 10 and len(filtered) < maximum_instances:
-            print("Adding instance")
-            sleep(10)
-            break
+    if f'web{vm_num}' in filtered['svname']:
+        print("Autoscaled one VM up")
+    else:
+        print(f"Autoscaling up failed for VM: web{vm_num}")
 
 
 if __name__ == "__main__":
-    autoscaling()
+    add_machine_to_pool(5)
 
 
         
